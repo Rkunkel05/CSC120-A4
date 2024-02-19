76b517f7f28e6046fb2f83eb82ec6c50c1203a75
Rachel <rkunkel@smith.edu>
2024-02-19 09:39:31 -0500

    Almost done w/ Car... error "this.onBoard" is null...

--- Car.java
+++ Car.java
@@ -1,44 +1,25 @@
-// Next, we'll set to work on the `Car` class. The `Car` class will need:
-
- // - an `ArrayList` where it will store the `Passenger`s currently onboard, and an `int` for the `Car`'s maximum capacity (since `ArrayList`s will expand as we add objects, we'll need to manually limit their size)
-//  - a constructor, which takes in an initial value for the `Car`'s maximum capacity and initializes an appropriately-sized `ArrayList`
-// - accessor-like methods `public int getCapacity()` 
-// and `public int seatsRemaining()` that return the maximum capacity and remaining seats, 
-// respectively- `addPassenger(Passenger p)` and `removePassenger(Passenger p)` 
-// methods to add or remove a `Passenger` from the `Car` and return `True` if the operation was 
-// successful, and `False` otherwise. (_Hint: don't forget to check that there are seats available 
-// if someone wants to board, and to confirm that the `Passenger` is actually onboard before trying to 
-// remove them! If you encounter a problem, you should `return False`._)
-
-// - and a final method `printManifest()` that prints out a list of all `Passenger`s aboard the car (or "This car is EMPTY." if there is no one on board).
-
-// As before, consider which of these should be `public` and which should be `private` (potentially with `accessor`s and/or `manipulator`s).
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Car {
     // Creates a list that stores the passangers
-    private List < String > onBoard = new ArrayList < > ();
+    private List<String> onBoard;
     // Set the maximum capacity as 20
     public int maxCapacity = 20;
     public int Capacity = maxCapacity;
+    int size = onBoard.size();
 
     // Makes car?
-    public Car(List < String > onBoard, int maxCapacity) {
+    public Car(List<String> onBoard, int maxCapacity) {
         this.onBoard = onBoard;
         this.maxCapacity = maxCapacity;
-        // Creates a certain amount of open spaces in the car
-        int i = 0;
-        while (i < maxCapacity) {
-            onBoard.add(" "); 
-            i += 1; }
-    }
+        this.size = onBoard.size();
+        }
 
     // Adds a passenger to the list onBoard
-    public boolean addPassenger(Passenger p) {
-        if (onBoard < 20) { 
-            onBoard.add(Passenger p);
+    public boolean addPassenger(String Passenger) {
+        if (size < maxCapacity) { 
+            onBoard.add(Passenger);
             Capacity -= 1;
             return true;
         }
@@ -48,9 +29,9 @@ public class Car {
         }
     }
 
-    public boolean removePassenger(Passenger p) {
-        if (onBoard > 0) {
-            onboard.remove(Passenger p);
+    public boolean removePassenger(String Passenger) {
+        if (size > 0) {
+            onBoard.remove(Passenger);
             Capacity += 1;
             return true;
         }
@@ -61,40 +42,40 @@ public class Car {
     }
 
     public void printManifest() {
-        public String passenger;
-        if onBoard > 0 {
-            for (passenger : onBoard); {
+        if (size > 0) {
+            for (String passenger : onBoard) {
                 System.out.println(passenger);
             }
         }
         else {
             System.out.println("This car is EMPTY.");
         }
-
     }
 
     // Returns the maximum capacity 
-    public int getCapacity() {
-        System.out.println(Capacity);
-        return Capacity;
+    public void getCapacity() {
+        System.out.println("Maximum capacity: " + Capacity);
     }
 
     // Returns how many seats are left
-    public int seatsRemaining() {
-        System.out.println(seatsRemaining);
-        return seatsRemaining;
+    public void seatsRemaining() {
+        int remainingSeats = maxCapacity - onBoard.size();
+        System.out.println("Seats remaining: " + remainingSeats);
     }
 
 
     public static void main(String[] args) {
-        Car myCar = new Car(<>, 20);
-        System.out.println("This car can hold", maxCapacity, "people");
+        Car myCar = new Car(new ArrayList<>(), 20);
+        myCar.getCapacity(); // Call the method using parentheses
+        myCar.seatsRemaining(); // Call the method using parentheses
         System.out.println("Adding passengers...");
         myCar.addPassenger("Sasha");
         System.out.println("The current manifest is:");
         myCar.printManifest();
-        System.out.println("Passenger leaving train...");
+        myCar.seatsRemaining(); // Call the method using parentheses
+        System.out.println("Passenger leaving car...");
         myCar.removePassenger("Sasha");
+        myCar.seatsRemaining(); // Call the method using parentheses
         System.out.println("The current manifest is:");
         myCar.printManifest();
     }
--- reflection.md
+++ reflection.md
@@ -1,4 +1,9 @@
 Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Encapsulation` affects your programming?
+* Very confusing still - I'm not entirely sure I **fully** understand how it all works, but I think its starting to come together a little bit...
+* I definietely might need some more clarification and guidance as we go ahead
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+* Google, TA hours, and office hours worked, as well as talking to other students in the class 
+* What didn't work was just staring at my computer - It helped to at least be breaking things down and writing something (even if it was just rewriting the instructions on paper) to make me feel productive and start getting in the mindset of working o this
+* DON'T JUMP STRAIGHT IN! Things will be very overwhelming if you do that. Instead read through the instructions and rubric a few times, write out a general plan, and then slowly start easing your way into things.
