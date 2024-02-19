f679c1e15cdd62a8988839938ba1ccc14bf1c091
Rachel <rkunkel@smith.edu>
2024-02-18 19:11:02 -0500

    Fixed bugs w/ Engine.java

--- Engine.java
+++ Engine.java
@@ -3,16 +3,18 @@ public class Engine {
     // Define variables
     // Private because it doesn't need to be altered 
     private double maxFuel = 100;
-    private String FuelType;
-    public Engine(String FuelType, int maxFuel) {
-        this.FuelType = FuelType;
+    private double currentFuel = maxFuel;
+    private String fuelType;
+
+    public Engine(String fuelType, int maxFuel) {
+        this.fuelType = fuelType;
         this.maxFuel = maxFuel;
     }
 
     // Checks if fuel > 0, train goes. If 0, train can't go
-    public boolean go(int fuel) {
-        if (fuel > 0) {
-            fuel = fuel - 10;
+    public boolean go() {
+        if (currentFuel > 0) {
+            currentFuel = currentFuel - 10;
             return true;
         } else {
             return false;
@@ -20,26 +22,28 @@ public class Engine {
     }
 
     // Prints the amount of fuel
-    public void fuelAmt(int Fuel) {
-        System.out.println(Fuel);
+    public void fuelAmt() {
+        System.out.println(currentFuel);
     }
 
-    // FUEL:
-    // Define type of fuel used
-    // Define max fuel value
-    // Function to check the current fuel level
-    // Refuel() - Reset fuel level -> Max
-    // Go() - Decrease fuel level + return true if fuel > 0, and false if fuel<0
-
-    // CONSTRUCTOR:
-    // Defines variables (:
+    public void Refuel() {
+        currentFuel = maxFuel;
+    }
 
     public static void main(String[] args) {
-        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
-        while (myEngine.go(maxFuel)) {
+        Engine myEngine = new Engine("ELECTRIC", 100); // Pass an int for maxFuel
+
+        while (myEngine.go()) { // Call go() method without any parameters
             System.out.println("Choo choo!");
+            System.out.println("Fuel levels are at...");
+            myEngine.fuelAmt();
         }
+
         System.out.println("Out of fuel.");
+        System.out.println("Refueling..");
+        myEngine.Refuel();
+        System.out.println("Fuel levels are at...");
+        myEngine.fuelAmt();
     }
 }
 
